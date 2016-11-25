e090e4b2252a2221b4bfdc7f998bb6939dc5dfa7
Hikigaya <140601064@student.beykent.edu.tr>
2016-11-25 15:22:47 +0200

    WIN THE BALL LIKE KUROKO

---
+++ Kuroko's Leona/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp.Common;
+
+namespace Kuroko_s_Leona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        private static void OnGameLoad(EventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
