12abe7a219d40382745661e13dbf5378bfb5a7f1
Hikigaya <140601064@student.beykent.edu.tr>
2016-11-26 23:35:19 +0200

    SOO COME ONN THENNNN

---
+++ Hinata's Taliyah/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hinata_s_Taliyah.Champions;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Hinata_s_Taliyah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        private static void OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName == "Taliyah")
+            {
+                new Taliyah();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
